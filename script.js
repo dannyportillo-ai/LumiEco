0bcc8bbc81ec6098178587e826161756f0a40a96
danny_portillo <danny.portillo@cun.edu.co>
2026-09-10 07:33:05 -0500

    archivos confirmados

---
+++ script.js
@@ -0,0 +1,23 @@
+let temperatura =  -5 ;
+
+if (temperatura < 10 ) {
+console.log( "Hace mucho frio" );
+}else if (temperatura <= 20) {
+    console.log( "Hace frio" );
+}else if (temperatura <= 30) {
+    console.log( "temperatura agradable" );
+} else {
+    console.log( " Hace mucho calor" );
+} 
+
+// 1. ¿Qué variable utilizaste para almacenar la temperatura?
+// La variable temperatura.
+
+// 2. ¿Qué operadores de comparación utilizaste?
+// Utilicé < (menor que) y <= (menor o igual que).
+
+// 3. ¿Cuántas condiciones necesita tu programa?
+// Necesita 3 condiciones con if, else if y else.
+
+// 4. ¿Qué función cumple else?
+// else ejecuta una instrucción cuando ninguna de las condiciones anteriores se cumple, "la temperatura es mayor hace mucho calor si la temperatua es mayor a 30 grados celcius".
